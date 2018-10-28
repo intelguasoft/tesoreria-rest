cd2a052d3817a895c5ad0a2e5e3dee9353987801
@intelguasoft <hnrdiaz@gmail.com>
2018-10-27 21:57:07 -0600

    Configuración de la conexion con MongoDB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ require('./config/config');
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 
 //parseamos la data application/x-www-form-urlencoded
@@ -82,6 +84,16 @@ app.delete('/usuarios/:id', (req, res) => {
     });
 });
 
+mongoose.connect('mongodb://localhost:27017/tesoreria', (err, res) => {
+
+    if (err) throw err;
+
+    console.log("Base de datos 'Online'");
+
+});
+
 app.listen(process.env.PORT, () => {
+
     console.log(`Escuchando el puerto: ${process.env.PORT}`);
+
 });
