bbdb3afd965e220a5a4fa51b8796380d57a5b63e
@intelguasoft <hnrdiaz@gmail.com>
2018-10-28 23:35:22 -0600

    Creación del modelo Persona y el controlador personas

--- server/server.js
+++ server/server.js
@@ -13,22 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //parseamos la data application/json
 app.use(bodyParser.json());
 
+app.use(require('./controllers/home'));
 app.use(require('./controllers/usuarios'));
-
-app.get('/', (req, res) => {
-    res.json({
-        owner: "@intelguasoft",
-        website: "https://www.intelguasoft.com",
-        description: "Rest API para el control de tesorería de iglesia en base a ofrendas, promesas, ventas y demás entradas de dinero.",
-        contact: {
-            phone: "+502 5931 4660",
-            email: "hnrdiaz@gmail.com",
-            facebook: "/intelguasoft",
-            twitter: "@intelguasoft"
-        }
-
-    });
-});
+app.use(require('./controllers/personas'));
 
 mongoose.connect('mongodb://localhost:27017/tesoreria', (err, res) => {
 
